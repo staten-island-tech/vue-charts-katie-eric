d2b86df1cb256bd0bb9eac22795829a6c3b58c6d
EricEZz <zhangeric343@gmail.com>
2024-03-06 10:15:00 -0500

    added API

---
+++ project/src/views/OptionsView.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            LeadingCause:[ ],
+        };
+    },
+    mounted: function(){
+        this.fetchData();
+    },
+    methods: {
+        fetchData: async function(){
+            try{
+                const results = await fetch(
+                    'https://data.cityofnewyork.us/resource/jb7j-dtam.json'
+                );
+                const data = await results.json();
+                this.LeadingCause = data.results
+            } catch(error){
+                console.log(error);
+            }
+            
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>

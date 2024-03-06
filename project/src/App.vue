ac0043618e2672d3139d17e54b3a35a8aa5f701e
EricEZz <zhangeric343@gmail.com>
2024-03-06 09:41:07 -0500

    da

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,26 +1,17 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import BarChart from './components/BarChart.vue';
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <BarChart />
 </template>
 
-<style scoped>
+<script>
+import BarChart from '../src/components/BarChart.vue'
+
+export default {
+  name: 'App',
+  components: { BarChart }
+}
+</script>
+
+<!-- <style scoped>
 header {
   line-height: 1.5;
   max-height: 100vh;
@@ -82,4 +73,4 @@ nav a:first-of-type {
     margin-top: 1rem;
   }
 }
-</style>
+</style> -->

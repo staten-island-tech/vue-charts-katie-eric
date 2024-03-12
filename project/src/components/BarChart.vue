274cbfcbe8ed48a60c117e838b5dd912213178d3
EricEZz <zhangeric343@gmail.com>
2024-03-12 10:21:00 -0400

    idk what im doing chat

--- project/src/components/BarChart.vue
+++ project/src/components/BarChart.vue
@@ -1,4 +1,42 @@
 <template>
+  <div class="container">
+    <Bar v-if="loaded" :data="chartData" />
+  </div>
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data: () => ({
+    loaded: false,
+    chartData: null
+  }),
+  async mounted () {
+    this.loaded = false
+    try { 
+       const response = await fetch ('https://data.cityofnewyork.us/resource/jb7j-dtam.json');
+       if (response.status !)
+    // try {
+    //   const { userlist } = await fetch('https://data.cityofnewyork.us/resource/jb7j-dtam.json')
+    //   this.chartdata = userlist
+
+    //   this.loaded = true
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
+</script>
+
+
+
+<!-- <template>
     <Bar
       id="my-chart-id"
       :options="chartOptions"
@@ -27,4 +65,4 @@
       }
     }
   }
-  </script>
+  </script> -->
--- project/src/views/OptionsView.vue
+++ project/src/views/OptionsView.vue
@@ -17,9 +17,7 @@ export default {
     methods: {
         fetchData: async function(){
             try{
-                const results = await fetch(
-                    'https://data.cityofnewyork.us/resource/jb7j-dtam.json'
-                );
+                const results = await fetch('https://data.cityofnewyork.us/resource/jb7j-dtam.json');
                 const data = await results.json();
                 this.LeadingCause = data.results
                 console.log(data);

99a5c2a65ce82ab1b6c3837e5fc868b66cb968ce
EricEZz <zhangeric343@gmail.com>
2024-03-13 10:24:55 -0400

    idk whats ahppening chat

--- project/src/views/OptionsView.vue
+++ project/src/views/OptionsView.vue
@@ -17,10 +17,11 @@ export default {
     methods: {
         fetchData: async function(){
             try{
-                const results = await fetch('https://data.cityofnewyork.us/resource/jb7j-dtam.json');
-                const data = await results.json();
-                this.LeadingCause = data.results
-                console.log(data);
+                const URL = 'https://data.cityofnewyork.us/resource/jb7j-dtam.json';
+                const response = await fetch(URL);
+                const datapoints = await response.json();
+                console.log(datapoints);
+                return (datapoints);
             } catch(error){
                 console.log(error);
             }

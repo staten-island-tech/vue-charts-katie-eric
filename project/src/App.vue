e76d484a9fff2264e0e7ed5e5d40f551c1f7643b
KatieT12 <144460461+KatieT12@users.noreply.github.com>
2024-03-14 10:47:11 -0400

    sdfasd

--- project/src/App.vue
+++ project/src/App.vue
@@ -19,7 +19,7 @@ import { RouterLink, RouterView } from 'vue-router'
   <h1 class="title">Causes of Death</h1> <br>
   <nav>
         <RouterLink to="/bar">Bar Chart</RouterLink>
-        <RouterLink to="/pie ">Pie Graph</RouterLink>
+        <RouterLink to="/pie ">Pie lkshdbfGraph</RouterLink>
         <RouterLink to="/options ">Options Graph</RouterLink>
       </nav>
       <br>

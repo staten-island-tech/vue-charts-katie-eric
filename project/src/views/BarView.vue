dfaa80afc4d64d00038568fad1cab336fad85a8b
EricEZz <zhangeric343@gmail.com>
2024-03-19 10:13:31 -0400

    idk whas happening chat

--- project/src/views/BarView.vue
+++ project/src/views/BarView.vue
@@ -6,7 +6,7 @@
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 import { Pie } from 'vue-chartjs'
 import * as chartConfig from '../components/BarChart.vue'
-
+import {ref} from "vue"
 ChartJS.register(ArcElement, Tooltip, Legend)
 
 export default {
@@ -15,7 +15,35 @@ export default {
     Pie
   },
   data() {
-    return chartConfig
+    return {
+      chartConfig,
+          LeadingCause:[ ],
+              count: 0,
+        };
+
+  }, 
+  
+    mounted: function(){
+        this.fetchData();
+    },
+  methods: {
+        fetchData: async function(){
+            try{
+                const results = await fetch(
+                    'https://data.cityofnewyork.us/resource/jb7j-dtam.json'
+                );
+                const data = await results.json();
+                this.LeadingCause = data.results
+                
+                const MalignantCount = ref('0');
+                data
+                .filter(element => element.leading_cause.includes ('Malignant'))
+                .forEach(element => console.log(MalignantCount.value++));
+                console.log(data);
+            } catch(error){
+                console.log(error);
+            }
+        },
   }
 }
 </script>
--- project/src/views/PieView.vue
+++ project/src/views/PieView.vue
@@ -4,11 +4,41 @@
 
 <script>
 import PieChart from '../components/PieChart.vue'
-
+import {ref} from "vue";
 
 export default {
   name: 'App',
-  components: { PieChart}
+  components: { PieChart},
+  
+  
+  data(){
+        return {
+            LeadingCause:[ ],
+            count: 0,
+        };
+    },
+    mounted: function(){
+        this.fetchData();
+    },
+  methods: {
+        fetchData: async function(){
+            try{
+                const results = await fetch(
+                    'https://data.cityofnewyork.us/resource/jb7j-dtam.json'
+                );
+                const data = await results.json();
+                this.LeadingCause = data.results
+                
+                const MalignantCount = ref('0');
+                data
+                .filter(element => element.leading_cause.includes ('Malignant'))
+                .forEach(element => console.log(MalignantCount.value++));
+                console.log(data);
+            } catch(error){
+                console.log(error);
+            }
+        },
+  }
 }
 
 </script>
